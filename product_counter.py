57022127dc7ab92adc151bd1b12bd49d279b150d
Joshua Simpson <joshwvs@gmail.com>
2015-09-27 12:30:10 -0400

    Add loop to calculate total unit cost.
    We've got all the products we want, so let's iterate over them and sum
    all their costs.

--- product_counter.py
+++ product_counter.py
@@ -12,3 +12,11 @@ with open('products.json') as data_file:
 df = pd.DataFrame(data['products'])
 
 wanted_products = df[df.product_type.isin(allowed_types)]
+
+total_cost = 0.0
+
+for product in wanted_products.variants:
+    for variant in product:
+        total_cost += float(variant['price'])
+
+print(total_cost)
