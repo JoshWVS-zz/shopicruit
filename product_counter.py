bfb6194effe2f537e25844a29c54510e9af35d60
Joshua Simpson <joshwvs@gmail.com>
2015-09-27 10:58:02 -0400

    Moving from naïve traversal to pandas solution.
    Alright, if I'm doing this, I want to do this right. I've never used
    pandas before, the but data frames seem similar enough to R to get going
    quickly.
    
    I'm opting to move to the pandas solution because I think it is more
    flexible, and will allow me to leverage more sophistical analytical
    tools easily.

--- product_counter.py
+++ product_counter.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 
 import json
+import pandas as pd
+import numpy as np
 
 with open('products.json') as data_file:    
     data = json.load(data_file)
 
-total_cost = 0.0
+df = pd.DataFrame(data)
 
-for unit in data['products']:
-    if unit['product_type'] in ["Wallet", "Lamp"]:
-        for item in unit['variants']:
-            print(unit['product_type'])
-            total_cost += float(item['price'])
-
-print(total_cost)
+print(df.head())
